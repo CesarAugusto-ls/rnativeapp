3f3874e465c50c0f581c91e7e18a37a8c47a4dd1
CesarAugusto <cesaraugusto.ls@hotmail.com>
2020-08-31 18:10:27 -0300

    add navigation/gesture-handler/linking

--- src/index.js
+++ src/index.js
@@ -1,31 +1,9 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text, StatusBar} from 'react-native';
 
-import './config/ReactotronConfig ';
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-});
+import './config/ReactotronConfig';
 
 const App = () => {
-    return (
-        <>
-            <StatusBar barStyle="dark-content" />
-            <SafeAreaView style={styles.container}>
-                <Text style={styles.welcome}>Welcome</Text>
-            </SafeAreaView>
-        </>
-    );
+    return <View></View>;
 };
 
 export default App;
