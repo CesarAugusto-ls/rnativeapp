d4fcb99322eaece6a22c9276e27c2ed8492ab962
CesarAugusto <cesaraugusto.ls@hotmail.com>
2020-08-31 20:02:23 -0300

    styled Main page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import {View} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import { Container, Form, Input, SubmitButton } from './styles';
 
 const Main = () => {
-    return <View />;
+    return (
+        <Container >
+            <Form>
+                <Input
+                    autoCorrect={false}
+                    autoCaptalize="none"
+                    placeholder="Adicionar usuário"
+                />
+                <SubmitButton>
+                    <Icon name="add" size={20} color="#fff" />
+                </SubmitButton>
+            </Form>
+        </Container >
+    )
 };
 
 export default Main;
