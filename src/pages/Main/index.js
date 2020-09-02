66939d548d248e6ebba1ba09c4af8b38c8498d81
CesarAugusto <cesaraugusto.ls@hotmail.com>
2020-09-02 19:26:59 -0300

    get users

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,14 +3,26 @@ import { Keyboard } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import api from '../../services/api';
 
-import { Container, Form, Input, SubmitButton } from './styles';
+import {
+    Container,
+    Form,
+    Input,
+    SubmitButton,
+    List,
+    User,
+    Avatar,
+    Name,
+    Bio,
+    ProfileButton,
+    ProfileButtonText,
+} from './styles';
 
 const Main = () => {
 
     const [newUser, setNewUser] = React.useState('');
     const [users, setUsers] = React.useState([]);
 
-    handleAddUser = async () => {
+    const handleAddUser = async () => {
         const response = await api.get(`/users/${newUser}`);
         const data = {
             name: response.data.name,
@@ -19,8 +31,10 @@ const Main = () => {
             avatar: response.data.avatar_url,
         }
 
-        setUsers(...users, data)
+        setUsers([...users, data])
         setNewUser('')
+
+        console.tron.log(users)
 
         Keyboard.dismiss();
     }
@@ -42,6 +56,23 @@ const Main = () => {
                     <Icon name="add" size={20} color="#fff" />
                 </SubmitButton>
             </Form>
+            <List
+                data={users}
+                keyExtractor={user => user.login}
+                renderItem={({ item }) => {
+                    return (
+                        <User>
+                            <Avatar source={{ uri: item.avatar }} />
+                            <Name>{item.name}</Name>
+                            <Bio>{item.bio}</Bio>
+
+                            <ProfileButton onPress={() => { }}>
+                                <ProfileButtonText>Ver perfil</ProfileButtonText>
+                            </ProfileButton>
+                        </User>
+                    )
+                }}
+            />
         </Container >
     )
 };
